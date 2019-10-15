4e002f2c1af8848a9aa53fb63aea59578c85c886
malteschilling <malteschilling@users.noreply.github.com>
2019-10-15 15:55:56 +0200

    02 on Representation Learning

---
+++ 02_RepresentationLearning/02_RepresentationLearning-deck.filtered.hs
@@ -0,0 +1,65 @@
+[HorizontalRule
+,Header 1 ("",[],[]) [Str "Overview",Space,Str "today"]
+,OrderedList (1,Decimal,Period)
+ [[Plain [Str "Representation",Space,Str "Learning"]]
+ ,[Plain [Str "Classification",Space,Str "of",Space,Str "Features",Space,Str "\8211",Space,Str "using",Space,Str "Support",Space,Str "Vector",Space,Str "Machines"]]
+ ,[Plain [Str "Dynamic",Space,Str "Features",Space,Str "\8211",Space,Str "Echo",Space,Str "State",Space,Str "Networks"]]]
+,HorizontalRule
+,Header 1 ("",[],[]) [Str "Recap",Space,Str "-",Space,Str "A",Space,Str "spectrum",Space,Str "of",Space,Str "intelligence"]
+,Para [RawInline (Format "html") "<img data-src=\"data/01/intelligence_spectrum.svg\" id=\"svg\">"]
+,Para [Str "Spectrum",Space,Str "of",Space,Str "different",Space,Quoted DoubleQuote [Str "levels"],Space,Str "of",Space,Str "intelligence",Space,Str "\8211",Space,Str "for",Space,Str "Machine",Space,Str "Learning",Space,Str "(following",Space,Cite [Citation {citationId = "cs221_2018", citationPrefix = [], citationSuffix = [], citationMode = NormalCitation, citationNoteNum = 0, citationHash = 1}] [Str "(Liang",Space,Str "2018)"],Str ")."]
+,HorizontalRule
+,Header 1 ("",["sub"],[]) [Str "Overview",Space,Str "common",Space,Str "ML",Space,Str "algorithms"]
+,Div ("",[],[])
+ [Plain [RawInline (Format "html") "<figure>",RawInline (Format "html") "<img data-src=\"https://miro.medium.com/max/1600/1*dYgEs2roROf3j2ANzkDHMA.png\" style=\"height:600px;width:auto;\" alt=\"How to decide which machine learning algorithm to choose for a problem [@cheatsheet2018].\" title=\"fig:\">",RawInline (Format "html") "<figcaption>",Str "How",Space,Str "to",Space,Str "decide",Space,Str "which",Space,Str "machine",Space,Str "learning",Space,Str "algorithm",Space,Str "to",Space,Str "choose",Space,Str "for",Space,Str "a",Space,Str "problem",Space,Cite [Citation {citationId = "cheatsheet2018", citationPrefix = [], citationSuffix = [], citationMode = NormalCitation, citationNoteNum = 0, citationHash = 2}] [Str "(",Str "\8220Choosing",Space,Str "the",Space,Str "Right",Space,Str "Estimator\8221",Space,Str "2018)"],Str ".",RawInline (Format "html") "</figcaption>",RawInline (Format "html") "</figure>"]]
+,HorizontalRule
+,Header 1 ("",["sub"],[]) [Str "Slide",Space,Str "on",Space,Str "Gradient",Space,Str "Boosting",Space,Str "-",Space,Str "place",Space,Str "in",Space,Str "overview"]
+,HorizontalRule
+,Header 1 ("",[],[]) [Str "Goals",Space,Str "for",Space,Str "Today"]
+,Div ("",["incremental"],[])
+ [BulletList
+  [[Plain [Str "Introduce",Space,Str "general,",Space,Str "advanced",Space,Str "Machine",Space,Str "Learning",Space,Str "techniques",Space,Str "and",Space,Str "their",Space,Str "application."]]
+  ,[Plain [Str "Explain",Space,Str "and",Space,Str "give",Space,Str "a",Space,Str "thorough",Space,Str "understanding",Space,Str "in",Space,Str "particular",Space,Str "of",Space,Str "Unsupervised",Space,Str "Learning",Space,Str "approaches."]]
+  ,[Plain [Str "Provide",Space,Str "an",Space,Str "overview",Space,Str "of",Space,Str "different",Space,Str "types",Space,Str "of",Space,Str "current",Space,Str "Machine",Space,Str "Learning",Space,Str "methods",Space,Str "and",Space,Str "their",Space,Str "characteristics."]]
+  ,[Plain [Str "Give",Space,Str "a",Space,Str "basic",Space,Str "introduction",Space,Str "into",Space,Str "(Deep)",Space,Str "Reinforcement",Space,Str "Learning",Space,Str "and",Space,Str "underlying",Space,Str "theoretical",Space,Str "foundation",Space,Str "as",Space,Str "well",Space,Str "as",Space,Str "current",Space,Str "limitations."]]]]
+,HorizontalRule
+,Header 1 ("",["section"],[("data-background-color","#2CA02C")]) [Str "Representation",Space,Str "Learning"]
+,HorizontalRule
+,Header 1 ("",[],[]) [Str "Features"]
+,BulletList
+ [[Plain [Str "autoencoder",Space,Str "as",Space,Str "an",Space,Str "example"]]
+ ,[Plain [Str "learning",Space,Str "for",Space,Str "multiple",Space,Str "tasks,",Space,Str "away",Space,Str "from",Space,Str "end-to-end"]]]
+,HorizontalRule
+,Header 1 ("",["section"],[("data-background-color","#2CA02C")]) [Str "Support",Space,Str "Vector",Space,Str "Machines"]
+,HorizontalRule
+,Header 1 ("",[],[]) [Str "Kernel",Space,Str "Trick"]
+,HorizontalRule
+,Header 1 ("",[],[]) [Str "SVM"]
+,HorizontalRule
+,Header 1 ("",["section"],[("data-background-color","#2CA02C")]) [Str "Reservoir",Space,Str "Computing"]
+,HorizontalRule
+,Header 1 ("",["sub"],[]) [Str "Reservoir",Space,Str "computing"]
+,HorizontalRule
+,Header 1 ("",[],[]) [Quoted DoubleQuote [Str "Homework"],Space,Str "Question"]
+,Div ("",["col30"],[])
+ [Para [RawInline (Format "html") "<img data-src=\"data/Discussion.png\" style=\"height:auto;width:300;\">"]]
+,Div ("",["col70"],[])
+ [Para [Str "Want",Space,Str "to",Space,Str "take",Space,Str "some",Space,Str "time",Space,Str "in",Space,Str "lecture",Space,Str "to",Space,Str "discuss",Space,Str "questions",Space,Str "relating",Space,Str "to",Space,Str "artificial",Space,Str "intelligence",Space,Str "and",Space,Str "machine",Space,Str "learning.",LineBreak,LineBreak]
+ ,Para [Str "For",Space,Str "next",Space,Str "week:",Space,Str "Looking",Space,Str "at",Space,Str "the",Space,Str "spectrum",Space,Str "of",Space,Str "intelligence"]
+ ,BulletList
+  [[Plain [Str "What",Space,Str "are",Space,Str "(in",Space,Str "your",Space,Str "opinion)",Space,Str "the",Space,Str "next",Space,Str "breakthroughs?"]]
+  ,[Plain [Str "What",Space,Str "are",Space,Str "implications",Space,Str "for",Space,Str "society?"]]]]
+,Para [RawInline (Format "html") "<img data-src=\"data/01/intelligence_spectrum.svg\">"]
+,HorizontalRule
+,Header 1 ("",[],[]) [Str "Further",Space,Str "announcements"]
+,Div ("",["incremental"],[])
+ [BulletList
+  [[Plain [Str "Remember,",Space,Str "there",Space,Str "is",Space,Str "no",Space,Str "exercise",Space,Str "tomorrow."]]
+  ,[Plain [Str "There",Space,Str "is",Space,Str "a",Space,Str "seminar",Space,Str "on",Space,Str "Deep",Space,Str "Learning",Space,Str "and",Space,Str "its",Space,Str "application",Space,Str "in",Space,Str "Artificial",Space,Str "Intelligence",Space,Str "(Dr.\160Andrew",Space,Str "Melnik)",Space,Str "-",Space,Str "will",Space,Str "work",Space,Str "on",Space,Str "current",Space,Str "literature:",Space,Str "392115",Space,Str "Deep",Space,Str "Learning",Space,Str "for",Space,Str "AI."]]]]
+,HorizontalRule
+,Header 1 ("",["unnumbered","biblio"],[]) [Str "References"]
+,Div ("refs",["references"],[])
+ [Div ("ref-cheatsheet2018",[],[])
+  [Para [Str "\8220",Str "Choosing",Space,Str "the",Space,Str "Right",Space,Str "Estimator.\8221",Space,Str "2018.",Space,Str "2018.",Space,Link ("",[],[]) [Str "https://scikit-learn.org/stable/tutorial/machine_learning_map/index.html"] ("https://scikit-learn.org/stable/tutorial/machine_learning_map/index.html",""),Str "."]]
+ ,Div ("ref-cs221_2018",[],[])
+  [Para [Str "Liang,",Space,Str "Percy.",Space,Str "2018.",Space,Str "\8220Artificial",Space,Str "Intelligence:",Space,Str "Principles",Space,Str "and",Space,Str "Techniques.\8221",Space,Str "Course",Space,Str "CS221,",Space,Str "Stanford",Space,Str "University,",Space,Str "Lecture",Space,Str "Notes."]]]]
